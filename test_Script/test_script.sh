71a4d116f0e5fe47c12e52553579a01bb2694093
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-03-02 23:00:08 -0500

    test cases work

--- test_Script/test_script.sh
+++ test_Script/test_script.sh
@@ -26,7 +26,7 @@ differences_paths=()
 cd Tests || exit 1  # Exit if cd fails
 # Loop through each subdirectory in "Tests"
 # for subdir in */; do
-for subdir in login logout refund sell add_credit create delete buy; do
+for subdir in login logout refund sell add_credit create delete buy all_user available_games; do
     subdir="${subdir%/}"
     # Change into the subdirectory
     cd "$subdir" || { echo "Failed to enter subdirectory: $subdir"; continue; }

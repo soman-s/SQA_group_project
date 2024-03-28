3403d9c767d0231d4fed1375f30bf5e9f43eb8eb
Soman <somansheikh786@gmail.com>
2024-03-28 05:08:02 -0400

    test for buy

--- server/tests/test_transaction_files.py
+++ server/tests/test_transaction_files.py
@@ -13,6 +13,7 @@ from transactions import process_delete
 from transactions import process_credit
 from transactions import process_sell
 from transactions import process_refund
+from transactions import process_buy
 
 
 def test_create():
@@ -115,6 +116,54 @@ def test_process_refund():
     captured_output = StringIO()
     sys.stdout = captured_output
     process_refund(transaction_line,games_collection,current_users)
+
+    captured_output_value = captured_output.getvalue()
+    sys.stdout = sys.__stdout__  # Restore stdout
+
+def test_process_buy():
+    # test prevents buy from occuring if it results in a negative balance for the buyer
+    transaction_line = "04 PUBG                      admin2          admin1          000030.00"
+    current_users = ["admin1          AA 000020.00", "admin2          AA 000500.00"]
+    games_collection = ["PUBG                      admin2         "]
+
+    # Redirect stdout to a StringIO object
+    captured_output = StringIO()
+    sys.stdout = captured_output
+
+    process_buy(transaction_line, current_users, games_collection)
+
+    # Get the captured output
+    captured_output_value = captured_output.getvalue()
+    sys.stdout = sys.__stdout__ 
+
+
+
+    # Test process_buy correctly processes the buy transaction
+    transaction_line = "04 PUBG                      admin2          admin1          000030.00"
+    current_users = ["admin1          AA 000500.00", "admin2          AA 000500.00"]
+    games_collection = ["PUBG                      admin2         "]
+
+    process_buy(transaction_line, current_users, games_collection)
+
+    # check if admin1 balance is decreased by game amount, admin2 balance is increased by game amount, and admin1 owns PUBG in games collection
+    assert "PUBG                      admin1" in games_collection
+    assert "admin1          AA 000470.00" in current_users
+    assert "admin2          AA 000530.00" in current_users
+
+
+
+    # test that a buy transaction doesn't happen twice
+    transaction_line = "04 PUBG                      admin2          admin1          000030.00"
+    current_users = ["admin1          AA 000500.00", "admin2          AA 000500.00"]
+    games_collection = ["PUBG                      admin2         "]
+
+    process_buy(transaction_line, current_users, games_collection)
+
+    # Redirect stdout to a StringIO object
+    captured_output = StringIO()
+    sys.stdout = captured_output
+
+    process_buy(transaction_line, current_users, games_collection)
 
     captured_output_value = captured_output.getvalue()
     sys.stdout = sys.__stdout__  # Restore stdout

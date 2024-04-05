52cdd0073d9cd8aab0a564269ccf7580ab616d56
Soman <somansheikh786@gmail.com>
2024-04-05 09:08:46 -0400

    added logic for search

--- Code/transactions.cpp
+++ Code/transactions.cpp
@@ -781,8 +781,7 @@ string transactions::search(string user_name, vector<string>& all_users, vector<
 {
   utils utility;
 
-  string user_type = user_file_process().get_user_type(all_users, user_name);
-  cout << user_type << endl;
+  string logged_in_user_type = user_file_process().get_user_type(all_users, user_name);
 
   string search_value;
 
@@ -798,20 +797,54 @@ string transactions::search(string user_name, vector<string>& all_users, vector<
   // search in current users
   for (int i = 0; i < all_users.size(); i++)
   {
-    string user_name = utility.convert_to_lower(all_users[i].substr(0,constants::MAX_USER_NAME_LENGTH));
-    // user_name = utility.convert_to_lower(user_name);
+    string current_user_name = utility.convert_to_lower(all_users[i].substr(0,constants::MAX_USER_NAME_LENGTH));
+    string current_user_name_type = user_file_process().get_user_type(all_users, current_user_name);
 
-    for (int j = 0; j < search_value.size(); j++)
+    // flag for preventing adding users multiple times to the search results
+    bool user_added = false;
+
+    for (int j = 0; j < search_value.size() && user_added == false; j++)
     {
       char character = search_value[j];
 
-      if (user_name.find(character) != std::string::npos)
+      if (current_user_name.find(character) != std::string::npos)
       {
-        search_results.push_back(user_name);
-        break;
+        // if logged in user is not an admin
+        if (logged_in_user_type != "AA")
+        {
+          // if there is an admin account found
+          if (current_user_name_type == "AA")
+          {
+            // check if the admin account found from the search result sells any games 
+            for (int k = 0; k < all_games.size(); k++)
+            {
+              string name = utility.convert_to_lower(all_games[k].substr(constants::MAX_GAME_NAME_LENGTH + 1, constants::MAX_USER_NAME_LENGTH));
+
+              // if the admin sells any games, add it to the search results
+              if (name == current_user_name)
+              {
+                search_results.push_back(name);
+                user_added = true;
+                break;
+              }
+            }
+          }
+          else
+          {
+            search_results.push_back(current_user_name);
+            user_added = true;
+          }
+        }
+        else
+        {
+          search_results.push_back(current_user_name);
+          user_added = true;
+        }
       }
     }
   }
+
+
 
   // search in available games
   for (int i = 0; i < all_games.size(); i++)
@@ -833,11 +866,20 @@ string transactions::search(string user_name, vector<string>& all_users, vector<
 
   // print search results
   cout << "search results:" << endl;
-  for (int i = 0; i < search_results.size(); i++)
+  if (search_results.size() > 0)
   {
-    cout << search_results[i] << endl;
+
+    for (int i = 0; i < search_results.size(); i++)
+    {
+      cout << search_results[i] << endl;
+    }
+
+    cout << endl;
   }
-  cout << endl;
+  else
+  {
+    cout << "No results" << endl;
+  }
 
   return constants :: SUCESS_OPTION;
 }

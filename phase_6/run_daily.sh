a2f29c358f80aa828705353e174ba4edc984fc54
Edmond <edmond.lee@ontariotechu.net>
2024-04-05 20:55:17 -0400

    added testing for phase 6

--- phase_6/run_daily.sh
+++ phase_6/run_daily.sh
@@ -2,13 +2,20 @@
 #!/bin/sh
 
 
+# before each run must copy back the original files
+# copy back orginal files
+# chmod +x copy_ori.sh(first time only)
+# dos2unix copy_ori.sh
+# ./copy_ori.sh
+
 # Script to process 1 days worth of daily merged_daily_transactions
 # To run DAILY SCRIPT
-# chmod +x run_daily.sh
+# chmod +x run_daily.sh (first time only)
 # dos2unix run_daily.sh
 # ./run_daily.sh day_1(sub with the folder you want to test)
 
 custom_program="front_end.exe"
+actual_output_file="../tests/$1/actual/outputs.bto"
 
 day_inputs=()
 
@@ -28,15 +35,23 @@ done
 # Go back to root directory
 cd ../..
 
+
+
+
+
 # Running Inputs with the front end
 echo "Running $1  Sessions: Front End"
 cd front_end
+
+
+# Clear the output file before the loop
+> "$actual_output_file"
 
 for input_file in "${day_inputs[@]}"; do
   if [ -f "$input_file" ]; then
     echo "Running input file: $input_file"
 
-    ./"$custom_program" < "$input_file" >/dev/null 2>&1
+    ./"$custom_program" < "$input_file">>"$actual_output_file"
     # ./"$custom_program" < "$input_file"
     sleep 0.75
   else
@@ -53,6 +68,7 @@ destination_folder="back_end/ori_log_files"
 
 
 rsync -av --delete "$source_folder/" "$destination_folder/">/dev/null 2>&1
+sleep 0.75
 
 
 echo "Processing Backend for $1"
@@ -60,35 +76,115 @@ cd back_end
 python main.py>/dev/null 2>&1
 # python main.py
 
+# moving back to root directory
 cd ..
 
-# Copying the modified files
-source_folder="back_end/mod_log_files"
+
 # Loop through all files in the directory
 for file in *; do
   if [ -f "$file" ]; then
     dos2unix "$file" >/dev/null 2>&1
   fi
 done
-
+# Copying the modified files
+source_folder="back_end/mod_log_files"
 destination_folder="front_end/log_files"
 
 rsync -av --delete "$source_folder/" "$destination_folder/">/dev/null 2>&1
+sleep 0.75
 
 
-# copying the data files to a directory
+# copying the data files to a  daily runs directoy
 source_folder=back_end/mod_log_files
 destination_folder=daily_runs_output_files/$1
 cp -rf "$source_folder" "$destination_folder" >/dev/null 2>&1
-sleep 0.5
+sleep 0.75
 
 source_folder="back_end/ori_log_files/merged_daily_transactions.etf"
 destination_folder="daily_runs_output_files/$1"
 
 cp -rf "$source_folder" "$destination_folder" >/dev/null 2>&1
-sleep 0.5
+sleep 0.75
 
-
-
-
+source_folder="back_end/ori_log_files/merged_daily_transactions.etf"
+destination_folder="tests/$1/actual/"
+cp -rf "$source_folder" "$destination_folder" >/dev/null 2>&1
+sleep 0.75
 echo "Done Processing Sessions: $1 "
+
+echo "Testing:$1"
+# Testing portion
+
+# Testing outputs
+summary_output_file="tests/test_summary/output_diff_summary.txt"
+output_diff_file="tests/$1/diff/diff_outputs.bto"
+actual_output_file="tests/$1/actual/outputs.bto"
+expected_output_file="tests/$1/expected/outputs.bto"
+
+# comparing the actual and expected output files
+{
+    echo "Expected:"
+    cat "$expected_output_file"
+    echo "Actual:"
+    cat "$actual_output_file"
+    echo "Differences:"
+    diff -Z "$expected_output_file" "$actual_output_file"
+} > "$output_diff_file"
+
+diff_output=$(diff -Z  "$actual_output_file" "$expected_output_file")
+if [ -n "$diff_output" ]; then
+  echo "Test: $1" >> "$summary_output_file"
+  echo "Fail: Expected Output and Actual Output Do Not Match" >> "$summary_output_file"
+  echo "Differences:" >> "$summary_output_file"
+  diff "$expected_output_file" "$actual_output_file" >> "$summary_output_file"
+  echo "===================================" >> "$summary_output_file"
+  echo -e "\e[31mFail: Expected Output and Actual Output Do Not Match\e[0m"
+
+
+
+else
+
+    echo "Test: $1" >> "$summary_output_file"
+    echo "Pass: Outputs match" >> "$summary_output_file"
+    echo "===================================" >> "$summary_output_file"
+    echo -e "\e[32mPass: Expected and Actual Output match\e[0m"
+fi
+
+
+# Testing merged transaction files
+summary_transaction_file="tests/test_summary/merged_transactions_diff_summary.txt"
+output_diff_transaction_file="tests/$1/diff/diff_transactions.etf"
+actual_transaction_file="tests/$1/actual/merged_daily_transactions.etf"
+expected_transaction_file="tests/$1/expected/merged_daily_transactions.etf"
+
+
+# comparing the actual and expected merged transaction files
+{
+    echo "Expected:"
+    cat "$expected_transaction_file"
+    echo "Actual:"
+    cat "$actual_transaction_file"
+    echo "Differences:"
+    diff "$expected_transaction_file" "$actual_transaction_file"
+} > "$output_diff_transaction_file"
+
+
+diff_output=$(diff "$actual_transaction_file" "$expected_transaction_file")
+if [ -n "$diff_output" ]; then
+  echo "Test: $1" >> "$summary_transaction_file"
+  echo "Fail: Expected Output and Actual Output Do Not Match" >> "$summary_transaction_file"
+  echo "Differences:" >> "$summary_transaction_file"
+  diff "$expected_transaction_file" "$actual_transaction_file" >> "$summary_transaction_file"
+  echo "===================================" >> "$summary_transaction_file"
+  echo -e "\e[31mFail: Expected Merged Transaction File and Actual Merged Transaction File Do Not Match\e[0m"
+
+
+
+
+else
+
+    echo "Test: $1" >> "$summary_transaction_file"
+    echo "Pass: Outputs match" >> "$summary_transaction_file"
+    echo "===================================" >> "$summary_transaction_file"
+    echo -e "\e[32mPass: Expected Merged Transaction File and Actual Merged Transaction File Match\e[0m"
+fi
--- phase_6/run_weekly.sh
+++ phase_6/run_weekly.sh
@@ -8,6 +8,7 @@ echo "Copying back the Original Log Files"
 dos2unix copy_ori.sh
 sleep 0.25
 ./copy_ori.sh
+sleep 0.25
 
 
 
@@ -16,6 +17,7 @@ echo "Compling Front End"
 cd front_end
 g++ -o front_end.exe main.cpp menus.cpp user_file_process.cpp constants.cpp games_file_process.cpp utils.cpp transactions.cpp
 cd ..
+sleep 0.75
 
 
 cd daily_sessions
@@ -30,8 +32,14 @@ for folder in *; do
     fi
 done
 
+# back in the root directory
 cd ..
 
+# Clearing Summary files before each run
+summary_output_file="tests/test_summary/output_diff_summary.txt"
+summary_transaction_file="tests/test_summary/merged_transactions_diff_summary.txt"
+> "$summary_output_file"
+> "$summary_transaction_file"
 
 for session_folder in "${daily_sessions[@]}"; do
   dos2unix run_daily.sh  >/dev/null 2>&1
@@ -42,12 +50,14 @@ for session_folder in "${daily_sessions[@]}"; do
   source_folder="back_end/mod_log_files"
   destination_folder="weekly_runs_output_files/$session_folder"
   cp -rf "$source_folder"/* "$destination_folder"/
-  sleep 0.5
+  sleep 0.75
 
   source_folder=back_end/ori_log_files/merged_daily_transactions.etf
   destination_folder=weekly_runs_output_files/$session_folder
 
   cp -f "$source_folder" "$destination_folder" >/dev/null 2>&1
-  sleep 0.5
+  sleep 0.75
 
 done
+
+echo "Done Processing Weekly Session"
